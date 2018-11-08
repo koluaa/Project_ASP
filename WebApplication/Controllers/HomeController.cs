b831eb6541e76ea911c8f6fc104a608047615043
Nikolay <n.kor150@outlook.com>
2018-11-08 08:05:46 +0500

    ...

---
+++ WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Serialization;
+using WebApplication.Models;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        static XmlSerializer formatter = new XmlSerializer(typeof(UserData));
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LoadXml(HttpPostedFileBase file)
+        {
+            var userData = (UserData)formatter.Deserialize(file.InputStream);
+            return View(userData);
+        }
+    }
+}
---
+++ WebApplication/Models/UserData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    [Serializable]
+    public class UserData
+    {
+        public string PassSeries { get; set; }
+        public string PassNumber { get; set; }
+        public string PassFullName { get; set; }
+        public DateTime PassReceivingDate { get; set; }
+        public string PassIss { get; set; }
+        public string PassAddress { get; set; }
+        public string LicenseSeries { get; set; }
+        public string LicenseNumber { get; set; }
+        public DateTime LicenseReceivingDate { get; set; }
+        public string[][] CarList { get; set; }
+    }
+}
